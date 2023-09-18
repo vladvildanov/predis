b5027bd88788be37991aeee345706e42a4ea083b
vladvildanov <divinez122@outlook.com>
2023-09-18 15:50:06 +0300

    Added cache object based on APCu extension

---
+++ src/Cache/ApcuCache.php
@@ -0,0 +1,151 @@
+<?php
+
+/*
+ * This file is part of the Predis package.
+ *
+ * (c) 2009-2020 Daniele Alessandri
+ * (c) 2021-2023 Till Krüss
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Predis\Cache;
+
+use APCUIterator;
+
+/**
+ * @group ext-apcu
+ */
+class ApcuCache implements CacheWithMetadataInterface
+{
+    /**
+     * @var APCUIterator
+     */
+    private $iterator;
+
+    public function __construct(APCUIterator $iterator = null)
+    {
+        $this->iterator = $iterator ?? new APCUIterator();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function add(string $key, $var, int $ttl = 0): bool
+    {
+        return apcu_add($key, $var, $ttl);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function store(string $key, $var, int $ttl = 0): bool
+    {
+        return apcu_store($key, $var, $ttl);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function batchAdd(array $dictionary, int $ttl = 0): array
+    {
+        return apcu_add($dictionary, null, $ttl);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function batchStore(array $dictionary, int $ttl = 0): array
+    {
+        return apcu_store($dictionary, null, $ttl);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function read(string $key)
+    {
+        return apcu_fetch($key);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function batchRead(array $keys)
+    {
+        return apcu_fetch($keys);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function delete(string $key): bool
+    {
+        return apcu_delete($key);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function batchDelete(array $keys)
+    {
+        return apcu_delete($keys);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function exists(string $key): bool
+    {
+        return apcu_exists($key);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function batchExists(array $keys): array
+    {
+        return apcu_exists($keys);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function flush(): bool
+    {
+        return apcu_clear_cache();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTotalSize(): int
+    {
+        return $this->iterator->getTotalSize();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTotalCount(): int
+    {
+        return $this->iterator->getTotalCount();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTotalHits(): int
+    {
+        return (int) apcu_cache_info(true)['num_hits'];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTotalMisses(): int
+    {
+        return (int) apcu_cache_info(true)['num_misses'];
+    }
+}
---
+++ src/Cache/CacheInterface.php
@@ -0,0 +1,109 @@
+<?php
+
+/*
+ * This file is part of the Predis package.
+ *
+ * (c) 2009-2020 Daniele Alessandri
+ * (c) 2021-2023 Till Krüss
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Predis\Cache;
+
+interface CacheInterface
+{
+    /**
+     * Add key-value pair into cache if it doesn't exist yet.
+     *
+     * @param  string $key
+     * @param         $var
+     * @param  int    $ttl
+     * @return mixed
+     */
+    public function add(string $key, $var, int $ttl = 0): bool;
+
+    /**
+     * Add key or overwrite an existing key value.
+     *
+     * @param  string $key
+     * @param         $var
+     * @param  int    $ttl
+     * @return bool
+     */
+    public function store(string $key, $var, int $ttl = 0): bool;
+
+    /**
+     * Batch version of add() operation.
+     *
+     * @param  array $dictionary Key-value pairs
+     * @param  int   $ttl
+     * @return array
+     */
+    public function batchAdd(array $dictionary, int $ttl = 0): array;
+
+    /**
+     * Batch version of store() operation.
+     *
+     * @param  array $dictionary Key-value pairs
+     * @param  int   $ttl
+     * @return array
+     */
+    public function batchStore(array $dictionary, int $ttl = 0): array;
+
+    /**
+     * Fetches value associated with given key.
+     *
+     * @param  string $key
+     * @return mixed
+     */
+    public function read(string $key);
+
+    /**
+     * Fetches values associated with given keys.
+     *
+     * @param  array $keys
+     * @return mixed
+     */
+    public function batchRead(array $keys);
+
+    /**
+     * Removes value associated with given key from cache.
+     *
+     * @param  string $key
+     * @return mixed
+     */
+    public function delete(string $key): bool;
+
+    /**
+     * Removes values associated with given keys from cache.
+     *
+     * @param  array $keys
+     * @return mixed
+     */
+    public function batchDelete(array $keys);
+
+    /**
+     * Checks if given key exists in the storage.
+     *
+     * @param  string $key
+     * @return bool
+     */
+    public function exists(string $key): bool;
+
+    /**
+     * Checks if given keys exists in the storage.
+     *
+     * @param  array $keys
+     * @return array
+     */
+    public function batchExists(array $keys): array;
+
+    /**
+     * Flushes cache.
+     *
+     * @return bool
+     */
+    public function flush(): bool;
+}
