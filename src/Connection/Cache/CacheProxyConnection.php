9227aea4536a82d8453386c346f3a9d567214de4
vladvildanov <divinez122@outlook.com>
2023-09-25 11:49:27 +0300

    Added phpstan ignore

--- src/Connection/Cache/CacheProxyConnection.php
+++ src/Connection/Cache/CacheProxyConnection.php
@@ -130,7 +130,7 @@ class CacheProxyConnection implements ConnectionInterface
      */
     public function getSentinelConnection(): NodeConnectionInterface
     {
-        /* @phpstan-ignore-line */
+        /* @phpstan-ignore-next-line */
         return $this->connection->getSentinelConnection();
     }
 
